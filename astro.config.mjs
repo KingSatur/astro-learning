66cd9628aa26366f822df75be34512a50f146e40
Juan Campillo <juancawool@outlook.com>
2024-01-23 21:03:33 -0500

    feat: add svelte support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
-import tailwind from '@astrojs/tailwind'
-import react from '@astrojs/react'
-import vue from '@astrojs/vue'
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
+import vue from '@astrojs/vue';
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), mdx(), react(), vue()],
-})
+  integrations: [tailwind(), mdx(), react(), vue(), svelte()]
+});
