4f1def51cff4ed1830f8c25248542c06a87a999a
Juan Campillo <juancawool@outlook.com>
2024-01-23 19:25:28 -0500

    feat: adding react support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
-import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), mdx()],
-})
+  integrations: [tailwind(), mdx(), react(), vue()]
+});
